ea532a5cbebd5c9255a504337bd03647491f682e
NewstartGit <SamuelRojas2066@gmail.com>
2023-12-20 15:12:33 -0500

    Added Roadrunner and began configuring it, much of it is red and I dont see any errors thusfar but it could be a potential problem.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CompetitionAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CompetitionAutonomous.java
@@ -18,23 +18,21 @@ public class CompetitionAutonomous extends LinearOpMode
     int pixelPosition = 0;
 
     @Override
-    public void runOpMode() throws InterruptedException
-    {
+    public void runOpMode() throws InterruptedException {
         mc.init(hardwareMap, true);
         //aTag.initAprilTag(hardwareMap);
         imu.initIMU(hardwareMap);
         cam.init(hardwareMap);
         //tensorflow.initTfod(hardwareMap);
 
-        mc.backClawClose(true,100);
-        mc.closeClaw(true,100);
+        mc.backClawClose(true, 100);
+        mc.closeClaw(true, 100);
 
         waitForStart();
 
         init_loop();
 
-        if(opModeIsActive())
-        {
+        if (opModeIsActive()) {
 
 
             //int pixelPosition = 0;
@@ -44,18 +42,14 @@ public class CompetitionAutonomous extends LinearOpMode
 
             //mc.liftSlide(.5,2400,4500);
 
-            mc.rotateArm(.7,3500);
+            mc.rotateArm(.7, 3500);
 
             //Scan
-            while(opModeIsActive())
-            {
-                if(pixelPosition == 0)
-                {
+            while (opModeIsActive()) {
+                if (pixelPosition == 0) {
                     //Update pixelPosition variable
                     pixelPosition = husky.runHusky();
-                }
-                else
-                {
+                } else {
                     break;
                 }
                 /*
@@ -76,50 +70,49 @@ public class CompetitionAutonomous extends LinearOpMode
 
             //mc.rotateArm(1,250);
 
-            switch(pixelPosition)
-            {
+            switch (pixelPosition) {
                 case 1:
-                    mc.drive(0,.75,5000,2000,true);
+                    mc.drive(0, .75, 5000, 2000, true);
                     break;
                 case 2:
                     //Move forward
-                    mc.drive(90,.75,1000,1000,true);
+                    mc.drive(90, .75, 1000, 1000, true);
                     //Move to align with pixel
-                    mc.drive(180,.5,2000,1700,true);
-                    mc.drive(0,0,500,0,false);
+                    mc.drive(180, .5, 2000, 1700, true);
+                    mc.drive(0, 0, 500, 0, false);
                     //Turn to face board (it tends to overshoot a lil bit)
-                    mc.rotate(95,.5,4000,imu);
+                    mc.rotate(95, .5, 4000, imu);
                     //Move to align with pixel
-                    mc.drive(0,.75,3000,2900,true);
-                    mc.drive(0,0,500,0,false);
+                    mc.drive(0, .75, 3000, 2900, true);
+                    mc.drive(0, 0, 500, 0, false);
                     //Open claw
-                    mc.backClawClose(false,1);
+                    mc.backClawClose(false, 1);
                     //Prevent weird glitch where claw movement will rotate bot
-                    mc.drive(0,0,100,0,false);
+                    mc.drive(0, 0, 100, 0, false);
                     //Move forward
-                    mc.drive(90,.75,1500,500,true);
-                    mc.drive(0,0,100,0,false);
+                    mc.drive(90, .75, 1500, 500, true);
+                    mc.drive(0, 0, 100, 0, false);
                     //Move left to align with center of april tags
-                    mc.drive(180,.75,1000,1500,true);
+                    mc.drive(180, .75, 1000, 1500, true);
                     break;
                 case 3:
                     //Move forward
-                    mc.drive(90,.75,1000,1000,true);
+                    mc.drive(90, .75, 1000, 1000, true);
                     //Turn to face board
-                    mc.rotate(90,.5,4000,imu);
+                    mc.rotate(90, .5, 4000, imu);
                     //Move to align with pixel
-                    mc.drive(0,.5,2000,2500,true);
-                    mc.drive(0,0,500,0,false);
+                    mc.drive(0, .5, 2000, 2500, true);
+                    mc.drive(0, 0, 500, 0, false);
                     //Back up into the pixel
-                    mc.drive(270,.75,1000,750,true);
-                    mc.drive(0,0,500,0,false);
+                    mc.drive(270, .75, 1000, 750, true);
+                    mc.drive(0, 0, 500, 0, false);
                     //Open claw
-                    mc.backClawClose(false,1);
+                    mc.backClawClose(false, 1);
                     //Prevent weird glitch
-                    mc.drive(0,0,200,0,false);
+                    mc.drive(0, 0, 200, 0, false);
                     //Move forward
-                    mc.drive(90,.75,1500,2000,true);
-                    mc.drive(0,0,100,0,false);
+                    mc.drive(90, .75, 1500, 2000, true);
+                    mc.drive(0, 0, 100, 0, false);
 
                     break;
                 default:
@@ -168,11 +161,13 @@ public class CompetitionAutonomous extends LinearOpMode
 
              */
         }
-
-
     }
+
+
+    
     //THIS NOT WORKING :((((
-    @Override
+    //@Override
+    /*
     public void init_loop()
     {
         if(pixelPosition == 0)
@@ -185,4 +180,6 @@ public class CompetitionAutonomous extends LinearOpMode
 
         }
     }
+
+     */
 }
